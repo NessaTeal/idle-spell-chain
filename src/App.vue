f048d512dcd7c37ccc1fe59c51992d75e7b72114
Vadim Prokopev <vadimjprokopev@gmail.com>
2020-04-19 05:48:30 +0300

    Extract Library

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
   <div id="app">
-    <Library />
+    <div class="outer-container">
+      <top-bar />
+      <shop />
+      <div class="inner-container">
+        <div class="left">
+          <library />
+        </div>
+        <div class="right">
+          <spell-chain />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Library from "./components/Library.vue";
+import Shop from "./components/Shop.vue";
+import SpellChain from "./components/SpellChain.vue";
+import SpellSlot from "./components/SpellSlot.vue";
+import TopBar from "./components/TopBar.vue";
 
 @Component({
   components: {
-    Library
+    Library,
+    Shop,
+    SpellChain,
+    SpellSlot,
+    TopBar
   }
 })
 export default class App extends Vue {
@@ -32,5 +51,21 @@ export default class App extends Vue {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.outer-container {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+}
+
+.inner-container {
+  display: flex;
+  width: 640px;
+  margin: auto;
+}
+
+.left {
+  flex: 1;
 }
 </style>
