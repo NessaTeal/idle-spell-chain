58ce840aca7ab80c75ab79e063f0a87bfc683ff4
Vadim Prokopev <Ishvarloiva@gmail.com>
2020-04-19 04:47:36 +0300

    Add entropy

--- src/classes/spells/SpellFactory.ts
+++ src/classes/spells/SpellFactory.ts
@@ -1,15 +1,9 @@
 import Spell from "./Spell";
-import { Multiply, Addition } from "../effects";
+import EffectFactory from "../effects/EffectFactory";
 
 export default class SpellFactory {
   static generateSpell(): Spell {
-    const random = Math.random();
-    if (random < 0.5) {
-      const secondRandom = Math.random() * 5 + 1;
-      return new Spell("Multiplier", new Multiply(secondRandom));
-    } else {
-      const secondRandom = Math.random() * 49 + 1;
-      return new Spell("Adder", new Addition(secondRandom));
-    }
+    const effect = EffectFactory.generateEffect();
+    return new Spell("New spell", effect);
   }
 }
