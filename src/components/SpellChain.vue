3d5c74dab99f546ea26fbc5bc34e2bf84e338761
Vadim Prokopev <Ishvarloiva@gmail.com>
2020-04-19 05:29:34 +0300

    Extract SpellChain

---
+++ src/components/SpellChain.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>Spell chain</h3>
+    <spell-slot
+      v-for="(spell, index) in chain.spells"
+      :spell="spell"
+      :key="index"
+      insertable="true"
+    />
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+import { State } from "vuex-class";
+import SpellSlot from "./SpellSlot";
+
+@Component({
+  components: {
+    SpellSlot
+  }
+})
+export default class Shop extends Vue {
+  @State chain;
+}
+</script>
+
+<style></style>
