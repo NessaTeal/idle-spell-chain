1a90f0ad0b13e0809da262ae8b0008404534c9d1
Vadim Prokopev <Ishvarloiva@gmail.com>
2020-04-19 05:48:30 +0300

    Extract Library

--- src/components/Library.vue
+++ src/components/Library.vue
@@ -1,64 +1,33 @@
 <template>
-  <div class="outer-container">
-    <top-bar />
-    <shop />
-    <div class="inner-container">
-      <div class="left">
-        <h3>Library</h3>
-        <div class="library">
-          <spell-slot
-            v-for="(spell, index) in allSpells"
-            :key="index"
-            :spell="spell"
-          />
-        </div>
-      </div>
-      <div class="right">
-        <spell-chain />
-      </div>
+  <div>
+    <h3>Library</h3>
+    <div class="library">
+      <spell-slot
+        v-for="(spell, index) in spells"
+        :key="index"
+        :spell="spell"
+      />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { State } from "vuex-class";
 import SpellSlot from "./SpellSlot.vue";
-import TopBar from "./TopBar.vue";
-import Shop from "./Shop.vue";
-import SpellChain from "./SpellChain.vue";
+import Spell from "@/classes/spells/Spell";
 
 @Component({
   components: {
-    SpellSlot,
-    TopBar,
-    Shop,
-    SpellChain
+    SpellSlot
   }
 })
 export default class Library extends Vue {
-  get allSpells() {
-    return this.$store.state.spells;
-  }
+  @State spells!: Spell[];
 }
 </script>
 
 <style>
-.outer-container {
-  display: flex;
-  flex-direction: column;
-  align-content: center;
-}
-
-.inner-container {
-  display: flex;
-  width: 640px;
-  margin: auto;
-}
-
-.left {
-  flex: 1;
-}
-
 .library {
   display: flex;
   flex-wrap: wrap;
